63c4b65755a8145597db84c478f45875c5c53f52
prabhu797 <prabhudesai007@gmail.com>
2024-08-07 14:00:43 +0530

    Setup for local machine

--- TaskHive/src/App.jsx
+++ TaskHive/src/App.jsx
@@ -1,38 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import { FrappeProvider } from 'frappe-react-sdk'
-function App() {
-  const [count, setCount] = useState(0)
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { useRoutes } from 'react-router-dom';
+import Router from './routes/Router';
 
+import { baselightTheme } from "./theme/DefaultColors";
+
+function App() {
+  const routing = useRoutes(Router);
+  const theme = baselightTheme;
   return (
-	<div className="App">
-	  <FrappeProvider>
-		<div>
-	  <div>
-		<a href="https://vitejs.dev" target="_blank">
-		  <img src="/vite.svg" className="logo" alt="Vite logo" />
-		</a>
-		<a href="https://reactjs.org" target="_blank">
-		  <img src={reactLogo} className="logo react" alt="React logo" />
-		</a>
-	  </div>
-	  <h1>Vite + React + Frappe</h1>
-	  <div className="card">
-		<button onClick={() => setCount((count) => count + 1)}>
-		  count is {count}
-		</button>
-		<p>
-		  Edit <code>src/App.jsx</code> and save to test HMR
-		</p>
-	  </div>
-	  <p className="read-the-docs">
-		Click on the Vite and React logos to learn more
-	  </p>
-	  </div>
-	  </FrappeProvider>
-	</div>
-  )
+    <ThemeProvider theme={theme}>
+
+      <CssBaseline />
+      {routing}
+
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
--- TaskHive/vite.config.js
+++ TaskHive/vite.config.js
@@ -2,18 +2,43 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react'
 import proxyOptions from './proxyOptions';
+import svgr from '@svgr/rollup'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [svgr(), react()],
 	server: {
 		port: 8080,
 		proxy: proxyOptions
 	},
 	resolve: {
 		alias: {
-			'@': path.resolve(__dirname, 'src')
+			'@': path.resolve(__dirname, 'src'),
+			src: path.resolve(__dirname, 'src'), // from config2
 		}
+	},
+	esbuild: {
+		loader: 'jsx',
+		include: /src\/.*\.jsx?$/,
+		exclude: [],
+	},
+	optimizeDeps: {
+		esbuildOptions: {
+			loader: {
+				'.js': 'jsx',
+			},
+			plugins: [
+				{
+					name: 'load-js-files-as-jsx',
+					setup(build) {
+						build.onLoad({ filter: /src\\.*\.js$/ }, async (args) => ({
+							loader: 'jsx',
+							contents: await fs.readFile(args.path, 'utf8'),
+						}));
+					},
+				},
+			],
+		},
 	},
 	build: {
 		outDir: '../teamlink/public/TaskHive',
